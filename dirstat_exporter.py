ecbcf7782534c7c8e1f5c87e13a4d8a7349f66ad
BogdanPetrov <ppdh@ya.ru>
2022-10-23 22:53:10 +0300

    Add total files size metric

--- dirstat_exporter.py
+++ dirstat_exporter.py
@@ -15,13 +15,21 @@ start_http_server(PORT)
 while True:
 
     files_count = 0
+    files_size_bytes = 0
     for item in SCAN_DIR.glob('*'):
         if item.exists() and item.is_file():
+            file_stat = item.stat()
             files_count += 1
+            files_size_bytes += file_stat.st_size
     
     metric_name = f"files_count"
     if metric_name not in metrics.keys():
         metrics[metric_name] = Gauge(metric_name, '', ['dir'])
     metrics[metric_name].labels(dir=SCAN_DIR.as_posix()).set(files_count)
+
+    metric_name = f"files_size_bytes"
+    if metric_name not in metrics.keys():
+        metrics[metric_name] = Gauge(metric_name, '', ['dir'])
+    metrics[metric_name].labels(dir=SCAN_DIR.as_posix()).set(files_size_bytes)
                 
     time.sleep(SLEEP_INTERVAL_SEC)
